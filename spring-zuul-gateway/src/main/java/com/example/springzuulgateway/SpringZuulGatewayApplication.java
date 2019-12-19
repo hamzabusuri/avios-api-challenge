c5162e39df1c5386dee519172a1a93287c27e73b
hb308 <hamzabusuri96@hotmail.com>
2019-12-19 15:14:29 +0000

    Adding zuul configuration

---
+++ spring-zuul-gateway/src/main/java/com/example/springzuulgateway/SpringZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springzuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-zuul-gateway/src/test/java/com/example/springzuulgateway/SpringZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springzuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
