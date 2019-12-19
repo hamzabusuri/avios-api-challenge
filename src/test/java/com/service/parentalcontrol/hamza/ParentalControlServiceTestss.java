b497448f24e9f1fd6dedf9355e7f4adba257ee3a
hb308 <hamzabusuri96@hotmail.com>
2019-12-19 12:32:13 +0000

    Adding basic ccircle ci pipeline

--- src/test/java/com/service/parentalcontrol/hamza/ParentalControlServiceTestss.java
+++ src/test/java/com/service/parentalcontrol/hamza/ParentalControlServiceTestss.java
@@ -1,5 +1,6 @@
 package com.service.parentalcontrol.hamza;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.service.parentalcontrol.hamza.exception.TechnicalFailureException;
 import com.service.parentalcontrol.hamza.exception.TitleNotFoundException;
 import com.service.parentalcontrol.hamza.model.MovieClassification;
@@ -10,13 +11,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+import static org.mockito.ArgumentMatchers.anyString;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParentalControlServiceTestss {
@@ -24,11 +31,19 @@ public class ParentalControlServiceTestss {
     @Mock
     private MovieService movieService;
 
+    @Mock
+    private DynamoDbRepository repo;
+
+    private DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
+
     @InjectMocks
     private ParentalControlServiceImpl parentalControlService;
 
 
-   /* @Test
+   /*@Test
     public void isMoviePermissible_permissibleMovies_expectIsPermissible() throws TitleNotFoundException, TechnicalFailureException {
 
         when(movieService.getParentalControlLevel("The Jungle Book")).thenReturn(movie.getIdentifier());
@@ -51,16 +66,18 @@ public class ParentalControlServiceTestss {
     /*@Test(expected = TitleNotFoundException.class)
     public void isMoviePermissible_movieNonExistent_expectTitleNotFoundException() throws TitleNotFoundException, TechnicalFailureException {
 
-        when(movieService.getParentalControlLevel("6")).thenThrow(TitleNotFoundException.class);
+        MovieClassification.MovieClassificationBuilder movie = MovieClassification.builder().movieId(MOVIE_ID).identifier("U");
+
+        Mockito.when(movieService.getParentalControlLevel("6")).thenThrow(TitleNotFoundException.class);
 
         parentalControlService.checkParentalControlLevel("6", "U");
     }
 
     @Test(expected = TechnicalFailureException.class)
     public void isMoviePermissible_runtimeFailure_expectTechnicalFailureException() throws TechnicalFailureException, TitleNotFoundException {
-
-        when(movieService.getParentalControlLevel("1")).thenThrow(TechnicalFailureException.class);
+        Mockito.when(movieService.getParentalControlLevel("1")).thenThrow(TechnicalFailureException.class);
 
         parentalControlService.checkParentalControlLevel("1", "***");
     }*/
+
 }
