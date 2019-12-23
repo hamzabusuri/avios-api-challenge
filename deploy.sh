ee8d8d2f358c68438ed35bef400cde79dcfbd820
hb308 <hamzabusuri96@hotmail.com>
2019-12-23 01:16:36 +0000

    Configuring auto deployment to PCS-APP beanstalk

--- deploy.sh
+++ deploy.sh
@@ -79,6 +79,8 @@ ZIP=$VERSION.zip
 
 echo Deploying $NAME to environment $STAGE, region: $REGION, version: $VERSION, bucket: $EB_BUCKET
 
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $REGION
 aws configure set default.output json
 
